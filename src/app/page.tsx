6e1a33742d4dc2b9c2b70d8a6b5391f6c94b2645
suryaprakasamc <suryaprakasam@innoboon.com>
2023-07-20 12:37:41 +0530

    vscode config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,3 @@
-import Image from 'next/image'
-
 export default function Home() {
-  return (
-   <h1>react practise project using next js
-   </h1>
-  )
+  return <h1>react practise project using next js</h1>;
 }
